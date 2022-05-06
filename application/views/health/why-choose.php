d9da99e704689724ebc66a7fc3d2252bc84b96a6
Nishant <densetek.nishant@gmail.com>
2022-05-06 12:04:48 +0530

    push

--- application/views/health/why-choose.php
+++ application/views/health/why-choose.php
@@ -7,26 +7,26 @@
     <div class="row why_choose_car_content">
       <div id="mediclaim_ins_crd_new" class="col-lg-4 col-md-12 col-sm-12 why_choose_car_con">
         <div id="new_add_crd" class="card crd_why_choose" style="width: 100%;">
-          <?= img("assets/front/img/icon/24-7.png", '', 'class="insr_img"'); ?>
-          <div class="card-body crd_bdy_ins">
+          <?= img("assets/front/img/icon/1.png", '', 'class="insr_img"'); ?>
+          <!-- <div class="card-body crd_bdy_ins">
             <h5 class="card-title crd_ins_h5">24*7 Support</h5>
-          </div>
+          </div> -->
         </div>
       </div>
       <div id="mediclaim_ins_crd_new" class="col-lg-4 col-md-12 col-sm-12 why_choose_car_con">
         <div id="new_add_crd" class="card crd_why_choose" style="width: 100%;">
-          <?= img("assets/front/img/icon/claim.png", '', 'class="insr_img"'); ?>
-          <div class="card-body crd_bdy_ins">
+          <?= img("assets/front/img/icon/3.png", '', 'class="insr_img"'); ?>
+          <!-- <div class="card-body crd_bdy_ins">
             <h5 class="card-title crd_ins_h5">Premium Claim Support</h5>
-          </div>
+          </div> -->
         </div>
       </div>
       <div id="mediclaim_ins_crd_new" class="col-lg-4 col-md-12 col-sm-12 why_choose_car_con">
         <div id="new_add_crd" class="card crd_why_choose" style="width: 100%;">
-          <?= img("assets/front/img/icon/paperless.png", '', 'class="insr_img"'); ?>
-          <div class="card-body crd_bdy_ins">
+          <?= img("assets/front/img/icon/4.png", '', 'class="insr_img"'); ?>
+          <!-- <div class="card-body crd_bdy_ins">
             <h5 class="card-title crd_ins_h5">Paperless Claims</h5>
-          </div>
+          </div> -->
         </div>
       </div>
     </div>
